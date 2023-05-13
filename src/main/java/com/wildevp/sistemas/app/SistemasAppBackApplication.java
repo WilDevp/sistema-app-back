01e5bf838c80e5d6fca00dfa309015d80cf3e40e
WilDevp <wilmar.garciava@gmail.com>
2023-05-13 13:51:15 -0500

    First commit

---
+++ src/main/java/com/wildevp/sistemas/app/SistemasAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.wildevp.sistemas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemasAppBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wildevp/sistemas/app/SistemasAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildevp.sistemas.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
